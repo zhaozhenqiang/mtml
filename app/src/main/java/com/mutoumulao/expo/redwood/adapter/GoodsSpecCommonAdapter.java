ec35b13d3b4a4e8af822faf80658901d4e892175
xiaoqiang <12609480@qq.com>
2018-09-17 16:04:34 +0800

    基本完工

--- app/src/main/java/com/mutoumulao/expo/redwood/adapter/GoodsSpecCommonAdapter.java
+++ app/src/main/java/com/mutoumulao/expo/redwood/adapter/GoodsSpecCommonAdapter.java
@@ -34,13 +34,23 @@ public class GoodsSpecCommonAdapter extends RecyclerView.Adapter<GoodsSpecCommon
     @"适用年龄",@"功效",@"品类",@"时间"
     * */
 
+    /*
+    乳白色  蓝色 黑色  白色 青色
+    银色  灰色  桔红色 西瓜红 粉红色
+    明黄色  荧光黄  金色 墨绿色 米白色
+    银色 深灰色 玫红色 红色 卡其色
+    杏色 香横色 浅黄色 翠绿色 绿色
+    天蓝色 花色 米黄色
+    *
+    * */
+
     private String[] typeArray2 = {"18", "19", "20", "21", "22", "23", "24", "25", "26","27","28"};
-    private String[] typeArray1 = {"尺寸", "材质", "型号", "颜色", "款式",
-            "器型", "口味", "色号", "适用人群", "容量",
-            "花型", "尺码", "地点", "香型", "货号",
-            "组合", "成份", "版本", "度数", "运营商",
-            "属性", "重量", "地区", "套餐", "类别",
-            "适用年龄", "功效", "品类", "时间"};
+    private String[] typeArray1 = {"乳白色", "蓝色", "黑色", "白色", "青色",
+            "银色", "灰色", "桔红色", "西瓜红", "粉红色",
+            "明黄色", "荧光黄", "金色", "墨绿色", "米白色",
+            "银色", "深灰色", "玫红色", "红色", "卡其色",
+            "杏色", "香横色", "浅黄色", "翠绿色", "绿色",
+            "天蓝色", "花色", "米黄色"};//, "时间"};
     boolean[] typeFlag = new boolean[typeArray1.length];
     public boolean moreFlag;
     //private List<StoreManagerListEntity.GuigesEntity> mSpecNameCommonList = new ArrayList<>();
